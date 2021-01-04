b6f07961f45e50ff7f4a12cb1840213353c39999
Sandra Vargas <sandra@Sandras-MacBook-Pro.local>
2021-01-04 01:40:53 -0800

    Add basic styles to toggle controls.

--- src/App.js
+++ src/App.js
@@ -21,46 +21,48 @@ const App = () => {
 
   return (
     <Fragment>
-      <nav>
-        <label>
-          Enable Traditional
-          <div className="switch">
-            <input
-              type="checkbox"
-              aria-label="Enable traditional style"
-              id="traditional_checkbox"
-              value={traditionalJapanese}
-              onChange={handleToggleTraditionalJapanese}
-            />
-            <span className="slider round"></span>
-          </div>
-        </label>
-        <label>
-          View English column
-          <div className="switch">
-            <input
-              type="checkbox"
-              aria-label="Enable English column"
-              id="english_checkbox"
-              value={showEnglishColumn}
-              onChange={handleToggleShowEnglishColumn}
-            />
-            <span className="slider round"></span>
-          </div>
-        </label>
-        <label>
-          View romaji column
-          <div className="switch">
-            <input
-              type="checkbox"
-              aria-label="Enable romaji column"
-              id="romaji_checkbox"
-              value={showRomajiColumn}
-              onChange={handleToggleShowRomajiColumn}
-            />
-            <span className="slider round"></span>
-          </div>
-        </label>
+      <nav className="site-header">
+        <section className="controls">
+          <label>
+            Enable Traditional
+            <div className="toggle">
+              <input
+                type="checkbox"
+                aria-label="Enable traditional style"
+                id="traditional_checkbox"
+                value={traditionalJapanese}
+                onChange={handleToggleTraditionalJapanese}
+              />
+              <span className="slider round"></span>
+            </div>
+          </label>
+          <label>
+            View English column
+            <div className="toggle">
+              <input
+                type="checkbox"
+                aria-label="Enable English column"
+                id="english_checkbox"
+                value={showEnglishColumn}
+                onChange={handleToggleShowEnglishColumn}
+              />
+              <span className="slider round"></span>
+            </div>
+          </label>
+          <label>
+            View romaji column
+            <div className="toggle">
+              <input
+                type="checkbox"
+                aria-label="Enable romaji column"
+                id="romaji_checkbox"
+                value={showRomajiColumn}
+                onChange={handleToggleShowRomajiColumn}
+              />
+              <span className="slider round"></span>
+            </div>
+          </label>
+        </section>
       </nav>
       <main className="App">
         { <PoemsView
