c5c70953431d520f5a8effd6d1eece3f8648c44c
Sandra Vargas <sandra@Sandras-MacBook-Pro.local>
2021-06-14 00:09:37 -0700

    Match colors to Material UI's palette

--- src/components/PoemContainer.js
+++ src/components/PoemContainer.js
@@ -1,29 +1,32 @@
 import {useContext} from 'react';
+import Paper from '@material-ui/core/Paper';
 import { valueToBoolean } from "../utils";
 import { PoemsContext } from '../PoemsContext';
 import PoemColumns from "./PoemColumns";
 
 const PoemContainer = ({ poem }) => {
-    const {
-        carouselView,
-        currentPoem,
-    } = useContext(PoemsContext);
-        
-    return (
-        <article className={`poem ${valueToBoolean(carouselView) ? poem.id === parseInt(currentPoem) + 1 ? 'active' : '' : ''}`} key={poem.id}>
-            <div className="scroll--unrolled">
-                <div className="poem__paper">
-                    <header className="poem__number"><h2 id={poem.id}>Poem {poem.id}</h2></header>
-                    <section className="poem__columns">
-                        <PoemColumns poem={poem} />
-                    </section>
-                </div>
-            </div>
-            <div className="scroll">
-                <div className="scroll--rolled"></div>
-            </div>
-        </article>
-    )
+  const {
+    carouselView,
+    currentPoem,
+  } = useContext(PoemsContext);
+    
+  return (
+    <article
+      className={`poem ${valueToBoolean(carouselView) ? poem.id === parseInt(currentPoem) + 1 ? 'active' : '' : ''}`}
+      key={poem.id}>
+      <div className="scroll--unrolled">
+        <Paper square={true} className="poem__paper">
+          <header className="poem__number"><h2 id={poem.id}>Poem {poem.id}</h2></header>
+          <section className="poem__columns">
+            <PoemColumns poem={poem} />
+          </section>
+        </Paper>
+      </div>
+      <div className="scroll">
+        <div className="scroll--rolled"></div>
+      </div>
+    </article>
+  )
 }
 
 export default PoemContainer;
